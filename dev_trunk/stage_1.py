c46f0e0af6cb67490b863abf2f70f60cf44eb4c8
josephwkania <60994292+josephwkania@users.noreply.github.com>
2025-05-28 17:46:02 +0100

    Update: Add JESS RFI mitigation to stage_1.py and stage_2.py; sort imports; reformat with Black.

--- dev_trunk/stage_1.py
+++ dev_trunk/stage_1.py
@@ -1,191 +1,232 @@
 #!/usr/bin/env python3.6
 import matplotlib
-matplotlib.use('Agg')
-from subprocess import PIPE, Popen
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-import logging
-import pysigproc
-from multiprocessing import Process
+
+matplotlib.use("Agg")
 import glob
-import numpy as np
-import pika
-from pika_send import send2Q
-from scipy.signal import savgol_filter
-from gpu_client import send2gpuQ
-from influx_2df import mjd2influx
+import logging
 import sys
 import threading
-from pika.exceptions import * #StreamLostError, ConnectionResetError
-from dump_all import tel_df_to_es
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from multiprocessing import Process
+from subprocess import PIPE, Popen
+
+import numpy as np
+import pika
+import pysigproc
 from elasticsearch import Elasticsearch
+from pika.exceptions import *  # StreamLostError, ConnectionResetError
+from scipy.signal import savgol_filter
+
+from dump_all_new import tel_df_to_es
+from gpu_client import send2gpuQ
+from influx_2df import mjd2influx
+from pika_send import send2Q
 from slack_send import *
 
 logger = logging.getLogger()
-format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=format)
-logging.getLogger('pika').setLevel(logging.INFO)
+logging.getLogger("pika").setLevel(logging.INFO)
 
-__author__='Devansh Agarwal'
-__email__ = 'da0017@mix.wvu.edu'
+__author__ = "Devansh Agarwal"
+__email__ = "da0017@mix.wvu.edu"
+
 
 def stage_initer(values):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
-    
-    channel.queue_declare(queue='stage01_queue', durable=True)
-    
+
+    channel.queue_declare(queue="stage01_queue", durable=True)
+
     def callback(ch, method, properties, body):
-        values.file=body.decode()
-        logging.info(f'got it {values.file}')
-        ch.basic_ack(delivery_tag = method.delivery_tag)
+        values.file = body.decode()
+        logging.info(f"got it {values.file}")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
         begin_main(values)
-        logging.info('Done')
-       # ch.basic_ack(delivery_tag = method.delivery_tag)
+        logging.info("Done")
+        # ch.basic_ack(delivery_tag = method.delivery_tag)
         logging.info("Ack'ed")
-    
+
     channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(on_message_callback=callback,
-                          queue='stage01_queue')
-    try:    
+    channel.basic_consume(on_message_callback=callback, queue="stage01_queue")
+    try:
         channel.start_consuming()
     except (StreamLostError, ConnectionResetError) as e:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
         channel = connection.channel()
 
+
 def mask_finder(data, sigma):
-    y = savgol_filter(data,61, 2)
-    mask = (data-y > sigma) | (data-y < -sigma)
+    y = savgol_filter(data, 61, 2)
+    mask = (data - y > sigma) | (data - y < -sigma)
     mask[:10] = True
     mask[980:1445] = True
     mask[3518:3560] = True
     return mask
 
 
-def _cmdline(command):	
+def _cmdline(command):
     """
     Function that captures output from screen
     """
-    process = Popen(args=command,stdout=PIPE,shell=True)
-    output=process.communicate()[0]
-    logging.info(f'Processed {command}')
+    process = Popen(args=command, stdout=PIPE, shell=True)
+    output = process.communicate()[0]
+    logging.info(f"Processed {command}")
     return output
 
-def write_and_plot(mask,chan_nos,freqs,bandpass,outdir):
+
+def write_and_plot(chan_nos, freqs, bandpass, outdir, mask=None):
     """
     Writes and plots the bandpass
     """
-    bad_chans=outdir+'bad_chans.flag'
-    bp_plot=outdir+'bandpass.png'
-    logging.info('Flagged %d channels',mask.sum())
-    with open(bad_chans,'w') as f:
-        np.savetxt(f,chan_nos[mask],fmt='%d',delimiter=' ', newline=' ')
+    bad_chans = outdir + "bad_chans.flag"
+    bp_plot = outdir + "bandpass.png"
+    if mask is not None:
+        logging.info("Flagged %d channels", mask.sum())
+        with open(bad_chans, "w") as f:
+            np.savetxt(f, chan_nos[mask], fmt="%d", delimiter=" ", newline=" ")
+    else:
+        with open(bad_chans, "w") as f:
+            pass
 
     import matplotlib.pyplot as plt
+
     fig = plt.figure()
     ax11 = fig.add_subplot(111)
-    ax11.plot(chan_nos,bandpass,'k-',label="Bandpass")
-    ax11.plot(chan_nos[mask],bandpass[mask],'ro',label="Flaged Channels")
+    ax11.plot(chan_nos, bandpass, "k-", label="Bandpass")
     ax11.set_xlabel("Chan. no.")
     ax11.set_ylabel("Arb. Units")
     ax21 = ax11.twiny()
-    ax21.plot(freqs[mask],bandpass[mask],'r.')
+    if mask is not None:
+        ax11.plot(chan_nos[mask], bandpass[mask], "ro", label="Flaged Channels")
+        ax21.plot(freqs[mask], bandpass[mask], "r.")
     ax21.invert_xaxis()
     ax21.set_xlabel("Frequency (MHz)")
     ax11.legend()
-    plt.savefig(bp_plot,bbox_inches='tight')
+    plt.savefig(bp_plot, bbox_inches="tight")
+
 
 def begin_main(values):
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     if values.verbose:
         logging.basicConfig(level=logging.DEBUG, format=format)
     else:
         logging.basicConfig(level=logging.INFO, format=format)
 
     try:
-        filterbank=glob.glob(values.file)[0]
+        filterbank = glob.glob(values.file)[0]
 
-
-
-        fil_obj = pysigproc.SigprocFile(filterbank) 
+        fil_obj = pysigproc.SigprocFile(filterbank)
         freqs = fil_obj.chan_freqs
         df = mjd2influx(fil_obj.tstart)
         if df is not None:
-            if len(df)  < 33:
+            if len(df) < 33:
                 send_msg_2_slack(f"Pointing info is missing!")
-            all_data_valid = df['DATA_VALID'].sum()
+            all_data_valid = df["DATA_VALID"].sum()
             if all_data_valid < 33:
-                logging.info('Less than 33s of data is valid, skipping this file')
-                _cmdline(f'rm {filterbank}')
+                logging.info("Less than 33s of data is valid, skipping this file")
+                _cmdline(f"rm {filterbank}")
                 return None
-            #else:
+            # else:
             #    es=Elasticsearch([{'host':'localhost','port':9200}])
             #    tel_df_to_es(es,df,filterbank)
         else:
             logging.info("Don't know what's going on!")
             send_msg_2_slack(f"No info from InfluxDB")
-            _cmdline(f'rm {filterbank}')
+            _cmdline(f"rm {filterbank}")
             return None
-        logging.info(f'{100*all_data_valid/len(df)}% data valid')
+        logging.info(f"{100*all_data_valid/len(df)}% data valid")
         bandpass = fil_obj.bandpass
-        chan_nos=np.arange(0,bandpass.shape[0])
-        mask=mask_finder(bandpass,values.sigma) #chan_nos,values.nchans,values.sigma)
-        bad_chans=chan_nos[mask]
+        chan_nos = np.arange(0, bandpass.shape[0])
+        # mask = mask_finder(
+        #     bandpass, values.sigma
+        # )  # chan_nos,values.nchans,values.sigma)
+        # bad_chans = chan_nos[mask]
 
-        frac_flagged=mask.sum()/4096
+        # frac_flagged = mask.sum() / 4096
+        frac_flagged = 0  # Get the real value from the output of jess
+        es = Elasticsearch([{"host": "localhost", "port": 9200}])
+        tel_df_to_es(es, df, filterbank, frac_flagged)
 
-        es=Elasticsearch([{'host':'localhost','port':9200}])
-        tel_df_to_es(es,df,filterbank, frac_flagged)
+        # out_chans = []
+        # for chans in bad_chans:
+        #     out_chans.append("-zap_chans")
+        #     out_chans.append(chans)
+        #     out_chans.append(chans)
 
-        out_chans=[]
-        
-        for chans in bad_chans:
-            out_chans.append('-zap_chans')
-            out_chans.append(chans)
-            out_chans.append(chans)
+        filterbank_name = filterbank.split("/")[-1].split(".")[0]
+        out_dir = "/ldata/trunk/{}/".format(filterbank_name)
+        _cmdline("mkdir -p {}".format(out_dir))
+        _cmdline(f"mv {filterbank} {out_dir}/")
+        new_fil_path = f"{out_dir}{filterbank_name}.fil"
+        clean_fil_path = f"{out_dir}{filterbank_name}_jb_4096.fil"
 
-        
-        filterbank_name=filterbank.split('/')[-1].split('.')[0]
-        out_dir='/ldata/trunk/{}/'.format(filterbank_name)
-        _cmdline('mkdir -p {}'.format(out_dir))
-        _cmdline(f'mv {filterbank} {out_dir}/')
-        new_fil_path=f'{out_dir}{filterbank_name}.fil'
+        jess_command = (
+            "time /sdata/miniconda/envs/py38/bin/python /opt/soft/jess/bin/jess_gauss.py"
+            + " -test jarque_bera -spb 4096 -mtz 1.5"
+            + f" -f {new_fil_path} -o {clean_fil_path}"
+        )
+        logging.info(f"Running {jess_command}")
+        send2gpuQ(jess_command)
 
-        heimdall_command='heimdall -nsamps_gulp 524288 -dm 10 10000 -boxcar_max 128 -cand_sep_dm_trial 200 -cand_sep_time 128 -cand_sep_filter 3 ' \
-                + ''.join(str(x)+' ' for x in out_chans) + ' -output_dir {}'.format(out_dir) + ' -f {}'.format(new_fil_path)
-        logging.info(f'Running {heimdall_command}')
+        heimdall_command = (
+            "heimdall -nsamps_gulp 524288 -dm 10 10000 -boxcar_max 128 -cand_sep_dm_trial 200 -cand_sep_time 128 -cand_sep_filter 3"
+            + " -rfi_no_broad"  #  -rfi_no_narrow
+            + " -output_dir {}".format(out_dir)
+            + " -f {}".format(clean_fil_path)
+        )
+        logging.info(f"Running {heimdall_command}")
 
-        p1 = Process(target = send2gpuQ,args=[heimdall_command])
+        p1 = Process(target=send2gpuQ, args=[heimdall_command])
         p1.start()
-        p2 = Process(target = write_and_plot,args=[mask,chan_nos,freqs,bandpass,out_dir])
+        p2 = Process(target=write_and_plot, args=[chan_nos, freqs, bandpass, out_dir])
         p2.start()
 
         p1.join()
         p2.join()
 
-        send2Q('stage02_queue', f'/ldata/trunk/{filterbank_name}')
+        send2Q("stage02_queue", f"/ldata/trunk/{filterbank_name}")
     except IndexError:
         pass
     return None
 
-if __name__ == '__main__':
-    parser=ArgumentParser(description='Stage 1: Get RFI Flags, run heimdall', formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Be verbose')
-    parser.add_argument('-d', '--daemon', dest='daemon', action='store_false', help='Run with AMQP')
-    parser.add_argument('-n', '--nchans', type=int, help='no. of chans to calc. median over', default=64)
-    parser.add_argument('-s', '--sigma', type=int, help='sigma over which values are tagged as RFI', default=5)
-    parser.add_argument('-f', '--file', type=str, help='Filterbank file')
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Stage 1: Get RFI Flags, run heimdall",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", help="Be verbose"
+    )
+    parser.add_argument(
+        "-d", "--daemon", dest="daemon", action="store_false", help="Run with AMQP"
+    )
+    parser.add_argument(
+        "-n", "--nchans", type=int, help="no. of chans to calc. median over", default=64
+    )
+    parser.add_argument(
+        "-s",
+        "--sigma",
+        type=int,
+        help="sigma over which values are tagged as RFI",
+        default=5,
+    )
+    parser.add_argument("-f", "--file", type=str, help="Filterbank file")
     parser.set_defaults(verbose=False)
     parser.set_defaults(daemon=True)
     values = parser.parse_args()
 
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     if values.verbose:
         logging.basicConfig(level=logging.DEBUG, format=format)
     else:
         logging.basicConfig(level=logging.INFO, format=format)
 
     if values.daemon:
-        logging.info('Running in daemon mode')
+        logging.info("Running in daemon mode")
         stage_initer(values)
     else:
         begin_main(values)
--- dev_trunk/stage_2.py
+++ dev_trunk/stage_2.py
@@ -2,159 +2,254 @@
 
 
 import matplotlib
-matplotlib.use('Agg')
-import threading
-import subprocess
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-import os
+
+matplotlib.use("Agg")
+import glob
 import logging
+import os
+import subprocess
+import sys
+import threading
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from multiprocessing import Process
+
 import numpy as np
 import pandas as pd
-from pandas.errors import EmptyDataError
-import pylab as plt
-import glob
 import pika
-from pika_send import send2Q
+import pylab as plt
 import pysigproc
-import sys
-from influx_2df import mjd2influx, extend_df
-from gpu_client import send2gpuQ
+from pandas.errors import EmptyDataError
 from pika.exceptions import *
+
+from gpu_client import send2gpuQ
+from influx_2df import extend_df, mjd2influx
+from pika_send import send2Q
 from slack_send import send_msg_2_slack
 
-__author__='Devansh Agarwal'
-__email__ = 'da0017@mix.wvu.edu'
+__author__ = "Devansh Agarwal"
+__email__ = "da0017@mix.wvu.edu"
 
 logger = logging.getLogger()
-format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=format)
-logging.getLogger('pika').setLevel(logging.INFO)
+logging.getLogger("pika").setLevel(logging.INFO)
+
 
 def stage_initer(values):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
 
-    channel.queue_declare(queue='stage02_queue', durable=True)
+    channel.queue_declare(queue="stage02_queue", durable=True)
 
     def callback(ch, method, properties, body):
-        values.files=body.decode() +  '/*cand'
-        logging.info(f'got it {values.files}')
-        ch.basic_ack(delivery_tag = method.delivery_tag)
+        values.files = body.decode() + "/*cand"
+        logging.info(f"got it {values.files}")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
         begin_main(values)
 
     channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(on_message_callback=callback, queue='stage02_queue')
-    
+    channel.basic_consume(on_message_callback=callback, queue="stage02_queue")
+
     try:
         channel.start_consuming()
     except (StreamLostError, ConnectionResetError) as e:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
         channel = connection.channel()
 
 
 def begin_main(values):
-    cand_lists=[]
+    cand_lists = []
     for files in glob.glob(values.files):
         try:
-            cand_lists.append(pd.read_csv(files, header=None,\
-                    comment='#',delim_whitespace=True,\
-                    names=['snr','sample','tcand','width','dm_trial','dm',\
-                    'members','start_sample','end_sample']))
+            cand_lists.append(
+                pd.read_csv(
+                    files,
+                    header=None,
+                    comment="#",
+                    delim_whitespace=True,
+                    names=[
+                        "snr",
+                        "sample",
+                        "tcand",
+                        "width",
+                        "dm_trial",
+                        "dm",
+                        "members",
+                        "start_sample",
+                        "end_sample",
+                    ],
+                )
+            )
         except EmptyDataError:
             pass
-    
+
+    base_work_dir = "/ldata/trunk"
+    folder = files.split("/")[-2]
+
+    fil_file = glob.glob(f"{base_work_dir}/{folder}/*jb_4096.fil")[0]
     if len(cand_lists) != 0:
-        cand_df = pd.concat(cand_lists,ignore_index=True)
-        mask_thresholds = (cand_df['snr']>= values.snr) & (cand_df['members']>= values.members) & (cand_df['tcand'] <= 504) &\
-                (cand_df['dm'] > values.dm) & (cand_df['width'] <= values.width)
+        cand_df = pd.concat(cand_lists, ignore_index=True)
+        mask_thresholds = (
+            (cand_df["snr"] >= values.snr)
+            & (cand_df["members"] >= values.members)
+            & (cand_df["tcand"] <= 504)
+            & (cand_df["dm"] > values.dm)
+            & (cand_df["width"] <= values.width)
+        )
 
-        base_work_dir = '/ldata/trunk'
-        folder=files.split('/')[-2]
-
-
-        fil_file = glob.glob(f'{base_work_dir}/{folder}/*.fil')[0]
         fil_obj = pysigproc.SigprocFile(fp=fil_file)
         mjd = fil_obj.tstart
         if len(cand_df[mask_thresholds]) > 0:
             influx_df = mjd2influx(mjd)
-            cand_df.loc[:,'cand_mjd'] = mjd + (cand_df['tcand']/(60*60*24))
-            extended_df  = extend_df(influx_df, cand_df[mask_thresholds])
-            mask = extended_df['cand_valid'] == 1
+            cand_df.loc[:, "cand_mjd"] = mjd + (cand_df["tcand"] / (60 * 60 * 24))
+            extended_df = extend_df(influx_df, cand_df[mask_thresholds])
+            mask = extended_df["cand_valid"] == 1
             cand_df_masked = extended_df[mask]
-        else: 
+        else:
             mask = mask_thresholds
             cand_df_masked = cand_df[mask]
-        
-        logging.info(f'Got {len(cand_df)} cands pre-filtering')
-        send_msg_2_slack(f'Got {len(cand_df)} cands pre-filtering at MJD {mjd}')
-        logging.info(f'Got {len(cand_df_masked)} cands post-filtering')
-        send_msg_2_slack(f'Got {len(cand_df_masked)} cands post-filtering at MJD {mjd}')
+
+        logging.info(f"Got {len(cand_df)} cands pre-filtering")
+        send_msg_2_slack(f"Got {len(cand_df)} cands pre-filtering at MJD {mjd}")
+        logging.info(f"Got {len(cand_df_masked)} cands post-filtering")
+        send_msg_2_slack(f"Got {len(cand_df_masked)} cands post-filtering at MJD {mjd}")
         if len(cand_df) != 0:
             plt.title(folder)
-            ax=cand_df.plot('tcand','dm',kind='scatter',\
-                    marker='.',c='none', edgecolors='k',s=cand_df.snr/1.0,alpha=0.5)
-            if len(cand_df_masked) !=0:
-                kill_mask = glob.glob(f'{base_work_dir}/{folder}/*.flag')
-                cand_df_masked.loc[:,'fil_file']= fil_file
-                cand_df_masked.loc[:,'kill_mask']= kill_mask[0]
-                cand_df_masked.plot('tcand','dm',kind='scatter',\
-                        marker='D',c='none', edgecolors='r',s=cand_df_masked.snr/1.0,ax=ax)
+            ax = cand_df.plot(
+                "tcand",
+                "dm",
+                kind="scatter",
+                marker=".",
+                c="none",
+                edgecolors="k",
+                s=cand_df.snr / 1.0,
+                alpha=0.5,
+            )
+            if len(cand_df_masked) != 0:
+                kill_mask = glob.glob(f"{base_work_dir}/{folder}/*.flag")
+                cand_df_masked.loc[:, "fil_file"] = fil_file
+                cand_df_masked.loc[:, "kill_mask"] = kill_mask[0]
+                cand_df_masked.plot(
+                    "tcand",
+                    "dm",
+                    kind="scatter",
+                    marker="D",
+                    c="none",
+                    edgecolors="r",
+                    s=cand_df_masked.snr / 1.0,
+                    ax=ax,
+                )
 
-                cand_df_masked.to_csv(f'{base_work_dir}/{folder}/{folder}.csv',index=False,
-                        header=True,columns=['fil_file','snr','tcand','dm','width','kill_mask',
-                                              'cand_mjd','ATAZIND','ATELIND','AZCORR','ELCORR','RA_deg',
-                                              'DEC_deg','RA_drift','DEC_drift','SCPROJID','WEBCNTRL','IFV1TNCI','ATRXOCTA',
-                                              'cand_gl','cand_gb','cand_ne2001','cand_ymw16','cand_valid'])
-            plt.xlabel('Time (s)')
-            plt.ylabel('DM (pc/cc)')
-            plt.savefig(f'{base_work_dir}/{folder}/{folder}.png', bbox_inches='tight')
+                cand_df_masked.to_csv(
+                    f"{base_work_dir}/{folder}/{folder}.csv",
+                    index=False,
+                    header=True,
+                    columns=[
+                        "fil_file",
+                        "snr",
+                        "tcand",
+                        "dm",
+                        "width",
+                        "kill_mask",
+                        "cand_mjd",
+                        "ATAZIND",
+                        "ATELIND",
+                        "AZCORR",
+                        "ELCORR",
+                        "RA_deg",
+                        "DEC_deg",
+                        "RA_drift",
+                        "DEC_drift",
+                        "SCPROJID",
+                        "WEBCNTRL",
+                        "IFV1TNCI",
+                        "ATRXOCTA",
+                        "cand_gl",
+                        "cand_gb",
+                        "cand_ne2001",
+                        "cand_ymw16",
+                        "cand_valid",
+                    ],
+                )
+            plt.xlabel("Time (s)")
+            plt.ylabel("DM (pc/cc)")
+            plt.savefig(f"{base_work_dir}/{folder}/{folder}.png", bbox_inches="tight")
             plt.close()
             if len(cand_df_masked) != 0:
-                cmd = f'mkdir -p {base_work_dir}/{folder}/cands'
+                cmd = f"mkdir -p {base_work_dir}/{folder}/cands"
                 subprocess.run(cmd.split(), stdout=subprocess.PIPE)
-                send2gpuQ(f'candmaker.py -n 11 -c {base_work_dir}/{folder}/{folder}.csv -o {base_work_dir}/{folder}/cands/')
-                send2gpuQ(f'predict.py -n 8 -b 64 -c {base_work_dir}/{folder}/cands/ -m a')
+                send2gpuQ(
+                    f"candmaker.py -n 11 -c {base_work_dir}/{folder}/{folder}.csv -o {base_work_dir}/{folder}/cands/"
+                )
+                send2gpuQ(
+                    f"predict.py -n 8 -b 64 -c {base_work_dir}/{folder}/cands/ -m a"
+                )
 
-                results = pd.read_csv(f'{base_work_dir}/{folder}/cands/results_a.csv')
-                label_mask = (results['label'] == 1)
-                send_msg_2_slack(f'Got {label_mask.sum()} cands post-FETCH at MJD {mjd}')
+                results = pd.read_csv(f"{base_work_dir}/{folder}/cands/results_a.csv")
+                label_mask = results["label"] == 1
+                send_msg_2_slack(
+                    f"Got {label_mask.sum()} cands post-FETCH at MJD {mjd}"
+                )
+                
                 if label_mask.sum() >= 1:
-                    files = results[label_mask]['candidate']
+                    files = results[label_mask]["candidate"]
                     for file in files:
                         send2Q("stage03_queue", file)
                 else:
                     delete_cmd = "put delete cmd here"
-                    #subprocess.run(cmd.split(), stdout=subprocess.PIPE)
-                    
+                    # subprocess.run(cmd.split(), stdout=subprocess.PIPE)
 
     else:
-        logging.info('No cands here!')
-
+        logging.info("No cands here!")
+    os.remove(fil_file)
 
     return None
 
-if __name__=='__main__':
 
-    parser=ArgumentParser(description='Stage 2: Sort Cands', formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Be verbose')
-    parser.add_argument('-d', '--daemon', dest='daemon', action='store_false', help='Run with AMQP')
-    parser.add_argument('-s', '--snr', type=int, help='sigma over which values are tagged as RFI', default=8.0)
-    parser.add_argument('-w', '--width', type=int, help = 'log 2 width of the candidates', default=7)
-    parser.add_argument('-D', '--dm', type=float, help = 'minimum DM to look out for', default=20)
-    parser.add_argument('-m', '--members', type=int, help='minimum number of members in the cluster', default=5)
-    parser.add_argument('-f', '--files', type=str, help='cand files')
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Stage 2: Sort Cands", formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", help="Be verbose"
+    )
+    parser.add_argument(
+        "-d", "--daemon", dest="daemon", action="store_false", help="Run with AMQP"
+    )
+    parser.add_argument(
+        "-s",
+        "--snr",
+        type=int,
+        help="sigma over which values are tagged as RFI",
+        default=8.0,
+    )
+    parser.add_argument(
+        "-w", "--width", type=int, help="log 2 width of the candidates", default=7
+    )
+    parser.add_argument(
+        "-D", "--dm", type=float, help="minimum DM to look out for", default=20
+    )
+    parser.add_argument(
+        "-m",
+        "--members",
+        type=int,
+        help="minimum number of members in the cluster",
+        default=5,
+    )
+    parser.add_argument("-f", "--files", type=str, help="cand files")
     parser.set_defaults(verbose=False)
     parser.set_defaults(daemon=True)
     values = parser.parse_args()
-    
+
     if values.verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
     if values.daemon:
-        logging.info('Running in daemon mode')
+        logging.info("Running in daemon mode")
         stage_initer(values)
     else:
         begin_main(values)
