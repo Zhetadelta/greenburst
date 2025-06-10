cbcac84f3b7c80dabdd5f55f9f593510efc6c9e9
Abyss Halley <abyss@kobold60.com>
2025-06-10 12:10:59 -0400

    first draft of h5 file processing

--- dev_trunk/database.py
+++ dev_trunk/database.py
@@ -1,15 +1,26 @@
 import sqlite3 as sql
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from astropy.time import Time
 from os import path, curdir
 import logging
 from psrqpy import QueryATNF
-from plot_cand import qpsr
+from plot_cand import qpsr, h5_loction_2_stuff
 
-def rowSort(psr):
+def psrRowSort(psr):
     """
-    Helper function which sorts astropy table row 'psr' according to the database schema order and places it into an ordered tuple.
+    Helper function which sorts astropy table row 'psr' according to the database schema order and returns an ordered list.
     """
-    return (psr['NAME'], psr['RAJ'], psr['DECJ'], psr['P0'], psr['DM'],psr['W50'],psr['W10'],psr['S1400'],psr['ASSOC'])
+
+    return [psr['NAME'], psr['RAJ'], psr['DECJ'], psr['P0'], psr['DM'],psr['W50'],psr['W10'],psr['S1400'],psr['ASSOC']]
+
+def tsRowSort(ts):
+    """
+    Helper function which sorts telescope dictionary 'ts' according to the database schema order and returns an ordered list.
+    """
+
+    logging.warn('Telescope elevation is not implemented yet. Not a problem, just be aware')
+    return [ts['MJD'], Time(ts['MJD'], format='mjd').iso, ts['Turret Angle (degree)'], None, ts['Receiver']]
+
 
 def connect(file):
     """
@@ -48,7 +59,7 @@ def schema(file):
     UTC text,
     Azimuth real,
     Elevation real,
-    Reciever text
+    Receiver text
     ); """
 
     if conn is not None:
@@ -56,52 +67,22 @@ def schema(file):
         c.execute(schemaString)
         conn.commit()
         conn.close()
-
-def searchForSourceToAdd(db, ra, dec):
-    conn = connect(db)
-    if conn is None:
-        logging.critical('Connect object failed')
-        return
-    c = conn.cursor()
-    storeString = """ INSERT INTO detections(Name, RAJ, DecJ, P0, DM, W50, W10, S1400, Assoc)
-    VALUES (?,?,?,?,?,?,?,?,?)
-    """
-
-    query, qTable = qpsr(ra, dec, params = ['NAME','RAJ', 'DECJ', 'P0', 'DM', 'W50', 'W10', 'S1400', 'ASSOC'])
-    qTable = query.table #reassign due to a hardcoded slice in original function
-    logging.info(f"Sources found: {len(qTable)}")
-    if len(qTable) > 0:
-        print(qTable)
-        psr = qTable[0]
-        c.execute(storeString, rowSort(psr))
-        conn.commit()
-    conn.close()
         
-def filteredSearch(db, ra, dec, epsilon=0.05, width=None, DM=None):
+def filteredSearch(ra, dec, epsilon=0.02, width=None, DM=None):
     """
-    Searches ATNF database for pulsars within 1 degree of coordinates with pulse width and/or DM conditions and appends to local database.
+    Searches ATNF database for pulsars within 1 degree of coordinates with pulse width and/or DM conditions.
 
     Positional arguments:
-    db (string) -- filename of database
-    ra (float) -- right ascention in degrees with decimals
-    dec (float) -- declination in degrees with decimals
+    ra (string) -- right ascention formatted as XXhXXmXXs
+    dec (string) -- declination formatted as XXdXXmXXs
 
     Keyword arguments:
     epsilon (float) -- factor of value to use as conditional window (default 0.05)
     width (float) -- width of pulse in milliseconds (default None)
     DM (float) -- Dispersion measure in pc/cm3 (default None)
 
-    Returns True if at least one source is found, and False otherwise.
-    """
-
-    conn = connect(db)
-    if conn is None:
-        logging.critical('Connect object failed')
-        return
-    c = conn.cursor()
-    storeString = """ INSERT INTO detections(Name, RAJ, DecJ, P0, DM, W50, W10, S1400, Assoc)
-    VALUES (?,?,?,?,?,?,?,?,?)
-    """
+    Returns a single astropy table row representing a found source if at least one is found, and False otherwise.
+    """ 
 
     #start by constructing search filter
     if width is not None:
@@ -119,17 +100,60 @@ def filteredSearch(db, ra, dec, epsilon=0.05, width=None, DM=None):
     logging.info(f"Sources found: {len(qTable)}")
     if len(qTable) > 0:
         print(qTable)
-        psr = qTable[0]
-        c.execute(storeString, rowSort(psr))
-        conn.commit()
-        conn.close()
-        return True
-    conn.close()
+        return qTable[0]
     return False
     
-    
-    
+def addRow(psr, ts=None):
+    """
+    Given a astropy table row 'psr' and h5 parameter dictionary 'ts', adds a database entry.
 
+    Positional arguments:
+    psr (dict) -- Unsorted astropy table row containing pulsar information
+
+    Keyword arguments:
+    ts (dict) -- Dictionary of parameters from h5 file (default None)
+
+    Returns True if row sucessfully added, False otherwise.
+    """
+
+    conn = connect(db)
+    if conn is None:
+        logging.critical('Connect object failed')
+        return False
+    c = conn.cursor()
+    storeString = """ INSERT INTO detections(Name, RAJ, DecJ, P0, DM, W50, W10, S1400, Assoc, MJD, UTC, Azimuth, Elevation, Receiver)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+    """
+
+    if ts is not None:
+        info = psrRowSort(psr) + tsRowSort(ts)
+    else:
+        info = psrRowSort(psr) + ([None]*5) #make sure they're the same length
+
+    c.execute(storeString, info)
+    conn.commit()
+    conn.close()
+    return True
+    
+def processh5File(h5File):
+    """
+    Given an h5 file, searches ATNF for a match and adds a row to database if one is found :3
+
+    Positional arguments:
+    h5File (string) -- Filename of an h5 file
+
+    Returns True if source is found and added to database, False if no source or database error.
+    """
+
+    stuff, params = h5_loction_2_stuff(h5File)
+    ra = params['RA (J2000)']
+    dec = params['DEC (J2000)']
+    dm = params['DM (pc/cc)']
+    result = filteredSearch(ra, dec, DM=dm) #search
+    if not result: #nothing found
+        logging.info('No known source in this region with this DM!')
+        return False
+    return addRow(result, ts=params)
     
 
 if __name__ == '__main__':
@@ -139,8 +163,10 @@ if __name__ == '__main__':
     #Example argument add: parser.add_argument('-f', '--files', nargs='+', help='Filterbank file')
     parser.add_argument('--db', dest='dbfile', help='Database file')
     parser.add_argument('-s', '--search', dest='searchString', help='Test search ra/dec/width')
+    parser.add_argument('-f', '--file', dest='h5File', help='h5 file to process and add to database')
     parser.set_defaults(dbfile=path.join(curdir,'test.db'))
     parser.set_defaults(searchString = None)
+    parser.set_defaults(h5File = None)
     parser.set_defaults(verbose=False)
     values = parser.parse_args()
 
@@ -152,6 +178,8 @@ if __name__ == '__main__':
 
     logging.debug(f"Accessing database at {values.dbfile}")
     schema(values.dbfile)
-    if values.searchString is not None:
+    if values.h5File is not None:
+        raise NotImplementedError #whoops
+    elif values.searchString is not None:
         ra, dec, w, dm = values.searchString.split(" ")
         filteredSearch(values.dbfile, float(ra), float(dec), epsilon=0.001, width=float(w), DM=float(dm))
