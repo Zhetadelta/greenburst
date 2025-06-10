8edcc43743e3687692438fd34b56c42e702c0398
Abyss Halley <abyss@kobold60.com>
2025-06-10 11:58:49 -0400

    modify qpsr to allow preformatted ra/dec strings

--- dev_trunk/plot_cand.py
+++ dev_trunk/plot_cand.py
@@ -14,7 +14,10 @@ import tqdm
 from psrqpy import QueryATNF
 
 def qpsr(sample_ra =6.02363,sample_dec =-72.08128, params = ['BNAME','JNAME', 'RAJ', 'DECJ','DM', 'S1400', 'P0'], condition = None):
-    (ra,dec)=deg2HMS(ra=sample_ra, dec=sample_dec)
+    if isinstance(sample_ra, str): #allow preformatted ra/dec
+        (ra,dec) = (sample_ra, sample_dec)
+    else:
+        (ra,dec)=deg2HMS(ra=sample_ra, dec=sample_dec)
     c=[ra,dec,1]
     #c=['00h24m05.67s','-72d04m52.62s',1]
     logging.info(f'Querying with  {c}')
