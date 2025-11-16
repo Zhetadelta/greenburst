5a80c7d439b0f857c47ddc73ecad68184cc985b6
Sakimori <sakimorihalley@gmail.com>
2025-11-16 12:10:39 -0500

    removed try/except

--- dev_trunk/stage_3.py
+++ dev_trunk/stage_3.py
@@ -39,27 +39,27 @@ def stage_initer(values):
     channel.start_consuming()
 
 def process_file(file_name, skip_pointing=False):
-    try:
-        if not skip_pointing:
-            if processh5File(file_name, None): #source with matching DM found
-                logging.info(f"Existing source found for file {file_name}")
-        fout, known_src = plotem(file_name, nrby=True, skip_pointing=skip_pointing) #modified: get nearby source info
-        file_name=fout.split('/')[-1][:-3]
-        if not skip_pointing:
-            with open(fout, 'rb') as f:
-                data = f.read()
-                response=dbx.files_upload(data,f'/plots/{file_name}png',mode=dropbox.files.WriteMode.overwrite)
-                logging.info(response)
-                link=dbx.sharing_create_shared_link(f'/plots/{file_name}png')
-                url=link.url
-                slack_send_url=re.sub(r"\&dl\=0", "&dl=1", url) # Replaced ? with & b/c change in dropbox url
-            logging.info(f'Create png at {fout}')
-            logging.info(f'Dropbox URL {slack_send_url}')
-            logging.info(send_img_2_slack(slack_send_url, nrby=known_src))
-        else: #if test mode
-            logging.info(f'Created png at {fout}')
-    except Exception as error:
-        logging.warning(f"Encountered {error} while processing file {file_name}")
+    #try:
+    if not skip_pointing:
+        if processh5File(file_name, None): #source with matching DM found
+            logging.info(f"Existing source found for file {file_name}")
+    fout, known_src = plotem(file_name, nrby=True, skip_pointing=skip_pointing) #modified: get nearby source info
+    file_name=fout.split('/')[-1][:-3]
+    if not skip_pointing:
+        with open(fout, 'rb') as f:
+            data = f.read()
+            response=dbx.files_upload(data,f'/plots/{file_name}png',mode=dropbox.files.WriteMode.overwrite)
+            logging.info(response)
+            link=dbx.sharing_create_shared_link(f'/plots/{file_name}png')
+            url=link.url
+            slack_send_url=re.sub(r"\&dl\=0", "&dl=1", url) # Replaced ? with & b/c change in dropbox url
+        logging.info(f'Create png at {fout}')
+        logging.info(f'Dropbox URL {slack_send_url}')
+        logging.info(send_img_2_slack(slack_send_url, nrby=known_src))
+    else: #if test mode
+        logging.info(f'Created png at {fout}')
+    #except Exception as error:
+        #logging.warning(f"Encountered {error} while processing file {file_name}")
 
 def begin_main(values):
     with open("config/conf.yaml", 'r') as stream:
